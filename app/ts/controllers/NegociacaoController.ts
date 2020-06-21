a736353b6aae03c5311ad6161e78471e89ae4cc5
dev-padawan <douglasmatosdev@gmail.com>
2020-06-21 17:47:15 -0300

    Creating class Negociacoes

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -20,6 +21,6 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao.volume)
+        this._negociacoes.adiciona(negociacao);
     }
 }
